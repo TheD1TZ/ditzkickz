19f28a62e3f0fd3d4754a9562b858485ec42b080
TheD1TZ <83147115+TheD1TZ@users.noreply.github.com>
2023-03-21 13:04:00 +0100

    Show Component

---
+++ src/components/ShowComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex bg-gray-800 rounded-2xl">
+    <h1 class="text-2xl px-4 bg-gray-800 rounded-2xl">{{ date }}</h1>
+    <img class="px-4 w-20" :src="getFlag(country)" />
+    <h1 class="text-2xl px-4">{{ description }}</h1>
+  </div>
+</template>
+
+<script>
+import getFlag from "../spotify/getFlag.js";
+import { countries } from "country-flag-icons";
+
+export default {
+  name: "ShowComponent",
+  methods: { getFlag },
+  props: {
+    date: String,
+    country: String,
+    description: String,
+  },
+};
+</script>
--- src/views/BookingsPage.vue
+++ src/views/BookingsPage.vue
@@ -1,21 +1,23 @@
 <template>
-
   <div class="flex content-center justify-center p-24 py-44 px-56 text-white">
-
     <div>
       <!--Booking list-->
       <div class="w-2/3">
         <h1 class="text-6xl py-5">UPCOMING BOOKINGS</h1>
-        <a class="text-3xl whitespace-normal">Take a look at the agenda and join the madness at one of my gigs.
-           Hope to see you soon!</a>
+        <a class="text-3xl whitespace-normal"
+          >Take a look at the agenda and join the madness at one of my gigs.
+          Hope to see you soon!</a
+        >
       </div>
 
       <div class="pt-8">
-        <div v-for="show in shows" class="flex justify-start py-2 ">
+        <div v-for="show in shows" class="flex justify-start py-2">
           <div class="flex bg-gray-800 rounded-2xl">
-            <h1 class="text-2xl px-4 bg-gray-800 rounded-2xl">{{ show.date }}</h1>
-            <img class="px-4 w-20" :src='getFlag(show.country)'>
-            <h1 class="text-2xl px-4">{{ show.description }}</h1>
+            <ShowComponent
+              :date="show.date"
+              :country="show.country"
+              :description="show.description"
+            ></ShowComponent>
           </div>
         </div>
       </div>
@@ -24,21 +26,17 @@
     <!--BOOK HERE-->
     <div class="w-1/4 bg-gray-700 rounded-2xl">
       <div class="text-center py-12">
-<!--        <img src="">-->
+        <!--        <img src="">-->
         <h1 class="text-white text-4xl font-logo">DITZKICKZ</h1>
         <h2 class="text-white text-3xl font-semibold">EMAIL ME FOR BOOKINGS</h2>
       </div>
     </div>
-
   </div>
 </template>
 
 <script>
-import { countries } from 'country-flag-icons'
-import getFlag from "../spotify/getFlag.js";
 export default {
   name: "BookingsPage",
-  methods: {getFlag},
   data() {
     return {
       shows: [
@@ -48,15 +46,24 @@ export default {
           description: "Defqon.1 Festival",
         },
         {
-          date: "2021-10-10",
+          date: "2021-12-10",
           country: "BE",
           description: "Mysteryland Festival",
-        }],
+        },
+        {
+          date: "2024-10-6",
+          country: "DE",
+          description: "Shutdown Festival",
+        },
+        {
+          date: "2021-10-10",
+          country: "NL",
+          description: "Rebirth",
+        },
+      ],
     };
   },
-}
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
